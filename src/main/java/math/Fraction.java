85ea37182762a0b3016fbf46e12ae7a6cbd70f62
Péter Jeszenszky <jeszenszky.peter.deik@gmail.com>
2020-05-07 12:46:27 +0200

    Implemented the two-argument constructor in the Fraction class

--- src/main/java/math/Fraction.java
+++ src/main/java/math/Fraction.java
@@ -33,9 +33,16 @@ public class Fraction extends Number implements Cloneable {
      * @throws ArithmeticException if the denominator is zero
      */
     public Fraction(int numerator, int denominator) throws ArithmeticException {
-        // TODO
-        this.numerator = numerator;
-        this.denominator = denominator;
+        if (denominator == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        if (denominator > 0) {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        } else {
+            this.numerator = -numerator;
+            this.denominator = -denominator;
+        }
     }
 
     /**
